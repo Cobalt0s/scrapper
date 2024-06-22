761e862026f0e8e4ce8d4c7b7be83cd170ac6700
Cobalt0s <constantin.y.koval@gmail.com>
2024-06-22 21:26:40 +0300

    gong: add different filters to keep modification operations

--- src/gong/index-modif.js
+++ src/gong/index-modif.js
@@ -7,12 +7,33 @@ async function main() {
     const links = await loadGetURLs(page);
 
     const payload = {
-      "data": links.filter(x => !x["name"].startsWith("get")),
+      "data": filterModificationOpp(links)
     };
 
-    await writeFile( "output/gong","index-modifications.json", payload)
+    await writeFile("output/gong", "index-modifications.json", payload)
 
   })
+}
+
+function filterModificationOpp(links) {
+  return links
+    .filter(x => !x["name"].startsWith("get"))
+}
+
+function filterCreateOppAndSingularObjectNames(links) {
+  return links
+    .filter(x => !x["name"].startsWith("get"))
+    .filter(x => !x["displayName"].startsWith("Retrieve"))
+    .filter(x => !x["displayName"].startsWith("List"))
+    .filter(x => !x["displayName"].startsWith("Update"))
+    .filter(x => !x["displayName"].startsWith("Delete"))
+    .filter(x => !x["name"].startsWith("delete"))
+    .filter(x => (x["name"].split("v2/")[1].match(/\//g) || []).length === 0) // no slashes after `v2/`
+}
+
+function filterDeleteOpp(links) {
+  return links
+    .filter(x => x["displayName"].startsWith("Delete") || x["name"].startsWith("delete"))
 }
 
 await main()
